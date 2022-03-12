69176b1d42c4f10f0b4b0db2c59715f35a08ffef
Elli Saarenmaa <47243975+ellisrnm@users.noreply.github.com>
2022-03-13 00:25:41 +0200

    exercise 1.9

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -21,19 +21,31 @@ const App = () => {
   const avg = (a, b, c) => (1*a + 0*b + -1*c)/(a+b+c)
   const pos = (a, b, c) => a/(a+b+c)*100
 
+  if (good + neutral + bad) {
+    return (
+      <div>
+        <Title text="give feedback" />
+        <Button handleClick={() => setGood(good + 1)} text="good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="bad" />
+        <Title text="statistics" />
+        <Statistics text="good" value={good} />
+        <Statistics text="neutral" value={neutral} />
+        <Statistics text="bad" value={bad} />
+        <Statistics text="all" value={good + neutral + bad} />
+        <Statistics text="average" value={avg(good, neutral, bad)} />
+        <Statistics text="positive" value={pos(good, neutral, bad)} unit="%" />
+      </div>
+    )
+  }
   return (
     <div>
-      <Title text="give feedback" />
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <Title text="statistics" />
-      <Statistics text="good" value={good} />
-      <Statistics text="neutral" value={neutral} />
-      <Statistics text="bad" value={bad} />
-      <Statistics text="all" value={good + neutral + bad} />
-      <Statistics text="average" value={avg(good, neutral, bad)} />
-      <Statistics text="positive" value={pos(good, neutral, bad)} unit="%" />
+    <Title text="give feedback" />
+    <Button handleClick={() => setGood(good + 1)} text="good" />
+    <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+    <Button handleClick={() => setBad(bad + 1)} text="bad" />
+    <Title text="statistics" />
+    <p>No feedback given</p>
     </div>
   )
 }
