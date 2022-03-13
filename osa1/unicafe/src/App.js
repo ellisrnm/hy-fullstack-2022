0ca24e75b9d2ff6d353727356002dbf87bea5456
Elli Saarenmaa <47243975+ellisrnm@users.noreply.github.com>
2022-03-13 16:15:25 +0200

    exercise 1.10

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -4,9 +4,24 @@ const Title = ({text}) => (
   <h1>{text}</h1>
 )
 
-const Statistics = (props) => (
+const StatisticLine = (props) => (
   <p>{props.text} {props.value} {props.unit}</p>
 )
+
+const Statistics = (props) => {
+  const avg = (a, c, d) => (a - c)/d
+  const pos = (a, d) => a/d*100
+  return(
+    <div>
+      <StatisticLine text="good" value ={props.good} />
+      <StatisticLine text="neutral" value ={props.neutral} />
+      <StatisticLine text="bad" value ={props.bad} />
+      <StatisticLine text="all" value={props.all} />
+      <StatisticLine text="average" value={avg(props.good, props.bad, props.all)} />
+      <StatisticLine text="positive" value={pos(props.good, props.all)} unit="%" />
+    </div>
+  )
+}
 
 const Button = (props) => (
   <button onClick={props.handleClick}>
@@ -18,32 +33,38 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const avg = (a, b, c) => (1*a + 0*b + -1*c)/(a+b+c)
-  const pos = (a, b, c) => a/(a+b+c)*100
+  const [all, setAll] = useState(0)
+  const handleGood = () => {
+    setGood(good + 1)
+    setAll(all +1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+    setAll(all +1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+    setAll(all +1)
+  }
 
-  if (good + neutral + bad) {
+  if (all) {
     return (
       <div>
         <Title text="give feedback" />
-        <Button handleClick={() => setGood(good + 1)} text="good" />
-        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-        <Button handleClick={() => setBad(bad + 1)} text="bad" />
+        <Button handleClick={handleGood} text="good" />
+        <Button handleClick={handleNeutral} text="neutral" />
+        <Button handleClick={handleBad} text="bad" />
         <Title text="statistics" />
-        <Statistics text="good" value={good} />
-        <Statistics text="neutral" value={neutral} />
-        <Statistics text="bad" value={bad} />
-        <Statistics text="all" value={good + neutral + bad} />
-        <Statistics text="average" value={avg(good, neutral, bad)} />
-        <Statistics text="positive" value={pos(good, neutral, bad)} unit="%" />
+        <Statistics good={good} neutral={neutral} bad={bad} all={all} />
       </div>
     )
   }
   return (
     <div>
     <Title text="give feedback" />
-    <Button handleClick={() => setGood(good + 1)} text="good" />
-    <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-    <Button handleClick={() => setBad(bad + 1)} text="bad" />
+    <Button handleClick={handleGood} text="good" />
+    <Button handleClick={handleNeutral} text="neutral" />
+    <Button handleClick={handleBad} text="bad" />
     <Title text="statistics" />
     <p>No feedback given</p>
     </div>
